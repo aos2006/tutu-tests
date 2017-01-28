8bb70b8c6e475bdbb7c46c305a9658010df2b2e0
anton <vashchenko-01@mail.ru>
2017-01-28 16:02:04 +0300

    fix

--- src/js/checkSyntax.js
+++ src/js/checkSyntax.js
@@ -1,70 +1,41 @@
 'use strict';
-let compose = (f, g) => (str, ex) => f(g(str, ex));
-let checkSequence = (arr) => {
-    let pairs = 0;
-    const iter = (list) => {
-      if (!list.length) return pairs;
-      return list.reduce((prev, item, i) => {
-        if (
-            `${prev}${item}` === '<>' ||
-            `${prev}${item}` === '[]' ||
-            `${prev}${item}` === '{}' ||
-            `${prev}${item}` === '()'
-        ) {
-          ++pairs;
-          list.splice(--i, 2);
-          iter(list);
-        }
-        return item;
-      });
-    }
-    iter(arr);
-    return pairs;
-}
-const sortBracketsStr = (str, excludedBrackets = []) => {
-  let bracketsList = new Map(
+var getOnlyBrackets = (str) => {
+  var regexp = /[^()\[\]{},<>]/g;
+  return (str).replace(regexp, "");
+};
+var areBracketsInOrder = (str) => {
+  let brackets = new Map(
       [
-        ['(', {car: ')', cdr: '('}],
-        [')', {car: '(', cdr: ')'}],
-        ['[', {car: ']', cdr: '['}],
-        [']', {car: '[', cdr: ']'}],
-        ['{', {car: '}', cdr: '{'}],
-        ['}', {car: '{', cdr: '}'}],
-        ['<', {car: '>', cdr: '<'}],
-        ['>', {car: '<', cdr: '>'}],
+          [']', '['],
+          ['}', '{'],
+          [')', '('],
+          ['>', '<']
       ]
   );
-  excludedBrackets.length > 0 ? excludedBrackets.forEach(item => {
-    bracketsList.delete(bracketsList.get(item).car);
-    bracketsList.delete(item);
-  }) : null;
-
-  return str.split('').filter(char => bracketsList.has(char));
-}
-
-const checkSyntax = (str, excludedBrackets = []) => {
-  let arrLen;
-  let result = compose(arr => (f) => {
-    arrLen = arr.length
-    return f(arr);
-  }, sortBracketsStr)(str, excludedBrackets);
-  let pairs = result(checkSequence);
-  if(!arrLen) return 0;
-  return arrLen / pairs === 2 ? 0 : 1;
-}
-
-console.log (checkSyntax ("---()[][]----") === 0);
-console.log (checkSyntax ("(") === 1);
-console.log (checkSyntax ("[]{}{}((()))") === 0);
+  let isClean = true;
+  const iter = (i, len, isCleaning, brackets, cur, acc) => {
+    if (i >= len && !isCleaning) return isCleaning && !acc.length;
+    if (i >= len) return isCleaning && !acc.length;
+    if (brackets.has(cur[i])) {
+      let isCleaning = acc.pop() === brackets.get(cur[i]);
+      return iter(++i, len, isCleaning, brackets, cur, acc);
+    } else {
+      acc.push(str[i]);
+      return iter(++i, len, isCleaning, brackets, cur, acc);
+    }
+  }
+  return iter(0, str.length, isClean, brackets, str, [])
+};
+var checkSyntax = (str) => {
+  const newStr = getOnlyBrackets(str);
+  return areBracketsInOrder(newStr) ? 0 : 1;
+};
+console.log (checkSyntax("---((++++))----") === 0);
+console.log (checkSyntax ("") === 0);
 console.log (checkSyntax ("before ( middle []) after ") === 0);
-console.log (checkSyntax ("( {} [] <> ){(((])))}") === 1);
-console.log (checkSyntax (") (", ['(']) === 0);
+console.log (checkSyntax (") (") === 1);
 console.log (checkSyntax ("} {") === 1);
-console.log (checkSyntax ("} {", ['{']) === 0);
-console.log (checkSyntax ("<(   >)") === 1);
-console.log (checkSyntax ("<(   >)", ['(']) === 0);
-console.log (checkSyntax ("(  [  <[]>  ()  ]  <>  )") === 0);
-console.log (checkSyntax ("(  [  <>  ()  ]  <>  )", ['<', '[', '(']) === 0);
+console.log (checkSyntax ("<>(])") === 1);
+console.log (checkSyntax ("(  [  <>  ()  ]  <>  )") === 0);
 console.log (checkSyntax ("   (      [)") === 1);
-console.log (checkSyntax ("   (      [)", ['[']) === 0);
 
